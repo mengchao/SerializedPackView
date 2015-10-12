2480de0aee525e9da652cd9ac879acfb44648c28
Chao Meng <ycm@qad.com>
2015-10-12 08:30:11 +0000

    Initial commit for Serialized Pack View

---
+++ SerializedPackViewApp/scripts/app.js
@@ -0,0 +1,8 @@
+(function () {
+    document.addEventListener("deviceready", function () {
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "nova"
+        });
+        navigator.splashscreen.hide();
+    });
+}());
