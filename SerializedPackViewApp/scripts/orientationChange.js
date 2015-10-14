eed898af8a1f5356807f296a5f1a73b1ad09f704
Frank Gong <fcg@qad.com>
2015-10-14 09:54:35 +0000

    Changes made by Frank Gong (fcg@qad.com).

--- SerializedPackViewApp/scripts/orientationChange.js
+++ SerializedPackViewApp/scripts/orientationChange.js
@@ -1,15 +1,19 @@
+
+var initialOriention = "endways";
 function doOnOrientationChange()
   {
     switch(window.orientation) 
     {  
       case -90:
       case 90:
+        initialOriention = "sidewards";
         if (document.getElementById("homePage").style.display != "block") {
             document.getElementById("packInfoPage").style.display="none";
             document.getElementById("packListPage").style.display="block";
         }
         break; 
       default:
+        initialOriention = "endways";
         if (document.getElementById("homePage").style.display != "block") {
             document.getElementById("packInfoPage").style.display="block";
             document.getElementById("packListPage").style.display="none";
